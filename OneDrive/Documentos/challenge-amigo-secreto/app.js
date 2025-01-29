c8a587212c599ee7c4d357f4cc06af2f9e47dd83
giovanep4mg <giovaneteodoro79@gmail.com>
2025-01-29 16:46:31 -0300

    teste-salvar

--- OneDrive/Documentos/challenge-amigo-secreto/app.js
+++ OneDrive/Documentos/challenge-amigo-secreto/app.js
@@ -4,3 +4,35 @@
 
 // Criar uma lista para guarda os nomes, vazia
 let listaAmigos = [];
+console.log(`lista criada vazia`);
+
+//Função para adicionar nomes a lista
+function adicionarAmigo(){
+
+    //
+    let nomeAmigo = document.getElementById('amigo').value;
+    console.log(`Nome adicionado: ${nomeAmigo}`);
+
+    //Adicionando nomes na lista
+    listaAmigos.push(nomeAmigo);
+    console.log(`Dados na lista: ${listaAmigos}`);
+
+    // limpar os campos digitados
+    document.getElementById('amigo').innerHTML = '';
+
+
+
+}
+
+
+
+// Mostrando um nome da lista
+//console.log(`Nome na posição 3: ${listaAmigos[2]}`);
+
+// Criando um número aleatório, para seleciona um nome
+//let numeroAleatorio = parseInt( Math.random() * 5 );
+
+//console.log(`Nome escolhido aleatório: ${listaAmigos[numeroAleatorio]}`);
+
+
+

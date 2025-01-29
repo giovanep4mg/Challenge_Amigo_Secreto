e05b7b1f4f5b1a6b393e66b5ae5038e85035aef8
giovanep4mg <giovaneteodoro79@gmail.com>
2025-01-29 17:58:29 -0300

    quase-finalizado

--- OneDrive/Documentos/challenge-amigo-secreto/app.js
+++ OneDrive/Documentos/challenge-amigo-secreto/app.js
@@ -2,37 +2,70 @@
 // Aqui você deverá desenvolver a lógica para resolver o problema.
 
 
-// Criar uma lista para guarda os nomes, vazia
+// Criar uma lista para guarda os nomes dos amigos mas está vazia
 let listaAmigos = [];
 console.log(`lista criada vazia`);
 
-//Função para adicionar nomes a lista
+//Função para adicionar nomes a lista de amigos
 function adicionarAmigo(){
 
-    //
+    // Pega o nome que o usuário digitou e salva na var 'nomeAmigo'
     let nomeAmigo = document.getElementById('amigo').value;
     console.log(`Nome adicionado: ${nomeAmigo}`);
 
-    //Adicionando nomes na lista
+    // Verificar se o nome foi preenchido corretamente ou não
+    if(nomeAmigo.trim() == ''){
+        alert("Erro! Preencha o campo nome!");
+        console.log("Campo nome não foi preenchido!");
+        return;
+    }
+
+    //Adiciona na lista o nome que foi adicionado antes na var 'nomeAmigo'
     listaAmigos.push(nomeAmigo);
-    console.log(`Dados na lista: ${listaAmigos}`);
 
-    // limpar os campos digitados
-    document.getElementById('amigo').innerHTML = '';
+    // Exibe o nome adicionado no console
+    console.log(`Nome adicionado: ${nomeAmigo}`);
+    console.log(`Lista atualizada: ${listaAmigos}`);
 
+    // Pega o nome que o usuário digitou e salva na var 'nomeAmigo'
+    document.getElementById('amigo').value = '';
+    console.log(`Limpando o campo de texto nome. ${nomeAmigo}`);
 
+    // Adicionar os nomes dos amigos na página
+    document.getElementById('listaAmigos').innerHTML = listaAmigos;
 
 }
 
+// Funçao sortear amigo
+function sortearAmigo(){
 
+    // Pega o valor de pessoas que tem na lista
+    let quantidade = listaAmigos.length;
+    console.log(`Quantidade dentro da lista: ${quantidade}`);
 
-// Mostrando um nome da lista
-//console.log(`Nome na posição 3: ${listaAmigos[2]}`);
+    // Verificar ->> Se a quantidade de amigos é maior que zero ou não
+    if(quantidade > 0 ){
 
-// Criando um número aleatório, para seleciona um nome
-//let numeroAleatorio = parseInt( Math.random() * 5 );
+        // Gerar um número aleatório de 0 há até a quantidade de amigos adicionados
+        let indiceSorteado = Math.floor( Math.random() * quantidade ); 
+        // Math.floor -> converte em número inteiro, redondando para baixo, isso garante que não dê erro no código.
 
-//console.log(`Nome escolhido aleatório: ${listaAmigos[numeroAleatorio]}`);
+        // Obtém o nome do amigo sorteado, na posição do indice
+        let resultadoSorteio = listaAmigos[indiceSorteado];
 
+        // Exibe o resultado no console
+        console.log(`Resultado do sorteio: ${resultadoSorteio}`);
 
+        // Adicionar o resultado na página
+        document.getElementById('resultado').innerHTML = resultadoSorteio;
+
+    } else {
+        console.log("Erro: A lista de amigos está vazia. Adicione nomes antes de sortear.");
+    }
+    
+}
+
+//
+// Chamar a função para testar
+sortearAmigo();
 

9325a42a81c33d654f7a0522dad32e2624d1a3e7
giovanep4mg <giovaneteodoro79@gmail.com>
2025-01-29 15:34:28 -0300

    criando o repositório

---
+++ OneDrive/Documentos/challenge-amigo-secreto/app.js
@@ -0,0 +1,6 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
+// Aqui você deverá desenvolver a lógica para resolver o problema.
+
+
+// Criar uma lista para guarda os nomes, vazia
+let listaAmigos = [];
